3c4ed1161c4fcb74137aa5c17cbb241c76fd626c
Ridercogswell <ridercogswell@gmail.com>
2022-05-18 22:44:03 -0700

    fixed webpack

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from 'react'; 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import SingleThought from './pages/SingleThought';
+// import NoMatch from './pages/NoMatch';
 
 const httpLink = createHttpLink({
-  // need am absolute path because react runs on port 3000 but the server runs on 3001, so we have to explicitly define its path
+  // need am absolute path because react runs on port 3000 but the server runs on 3001, so we have to explicitly define its path for development
   // uri: 'http://localhost:3001/graphql',
   // for production,
   uri: '/graphql',
@@ -18,19 +24,44 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-function App() {
+const App = () => {
   return (
-    // pass in info through client prop
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+              <Route
+                path="/profile"
+                element={<Profile />}
+              />
+              <Route
+                path="/thought"
+                element={<SingleThought />}
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
 
+
 export default App;
+
+
